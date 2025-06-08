3a067a58b6198e07522ceab085e9bc22871efb55
AlphaLynx <alphalynx@protonmail.com>
2025-06-08 03:10:00 -0400

    Add pull subtrees script

---
+++ pull-all-subtrees.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+subtrees=(
+    aircrack-ng-git
+    archivemount-ng
+    purrcrypt
+    pyactivate
+    sshp
+    sshp-git
+    ugrd-git
+)
+
+for pkg in "${subtrees[@]}"; do
+    echo "Pulling subtree '$pkg'..."
+    git subtree pull --prefix="$pkg" "https://aur.archlinux.org/$pkg.git" master
+done
